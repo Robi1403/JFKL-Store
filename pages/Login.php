51f66eaab68fa83d3eab3658e808372bd6faba36
Robby <rodejojohnrobert@gmail.com>
2024-04-30 23:16:14 +0800

    upadted some things

---
+++ pages/Login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <form action="" method="">
+            <h3>Login</h3>
+            
+            <input type="text" id="email" name="email" placeholder="User ID"><br><br>
+            
+            <input type="password" id="password" name="password" placeholder="Password"><br><br>
+
+            <button type="submit" id="submit">Sign In</button>
+            
+            <div id="logo">
+                <img src="../assets/storeLogo.svg" alt="">
+                <p>JFKL Store</p>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
