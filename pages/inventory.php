1bbd16831781c8332157117f3e3f0f9d233f5152
Hediphone <hediphonextra@gmail.com>
2024-05-04 15:30:15 +0800

    removed unnecessary lines

--- pages/inventory.php
+++ pages/inventory.php
@@ -1,5 +1,3 @@
-<!-- naga select gabos but su value sala -->
-
 <?php
 include ("PhpFunctions/connection.php");
 include ("PhpFunctions/remove_product.php");
@@ -132,29 +130,20 @@ include ("PhpFunctions/update_product.php");
                         $result = mysqli_query($conn, $select_query);
 
                         if ($result->num_rows > 0) {
-                            while ($row = $result->fetch_assoc()) {
-                                ?>
-                                <form name="productTable" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
-                                    method="POST">
+                            while ($row = $result->fetch_assoc()) { ?>
+                                <form name="productTable" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
                                     <tr>
-                                        <td><input type="checkbox" name="selectedProducts[]"
-                                                value="<?php echo $row["product_id"]; ?>"></td>
+                                        <td><input type="checkbox" name="selectedProducts[]" value="<?php echo $row["product_id"]; ?>"></td>
                                         <td><?php echo $row["product_id"]; ?></td>
                                         <td><?php echo $row["product_name"]; ?></td>
-                                        <?php
-                                        if ($row["net_weight"] == NULL) { ?>
-                                            <td>-</td>
-                                            <?php
-                                        } else { ?>
-                                            <td><?php echo $row["net_weight"]; ?></td> <!-- Net Weight -->
-                                            <?php
-                                        } ?>
+                                        <td><?php echo $row["net_weight"] ?? '-'; ?></td>
                                         <td><?php echo $row["category"]; ?></td>
                                         <td><?php echo $row["unit_price"]; ?></td>
                                         <td><?php echo $row["retail_price"]; ?></td>
                                         <td><?php echo $row["stock"]; ?></td>
                                         <td>
                                             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                                                <!-- hidden inputs for the UpdateProductModal with product information -->
                                                 <input type="hidden" name="productId_info" value="<?php echo $row["product_id"]; ?>">
                                                 <input type="hidden" name="productName_info" value="<?php echo $row["product_name"]; ?>">
                                                 <input type="hidden" name="netWeight_info" value="<?php echo $row["net_weight"]; ?>">
@@ -164,8 +153,7 @@ include ("PhpFunctions/update_product.php");
                                                 <input type="hidden" name="retailPrice_info" value="<?php echo $row["retail_price"]; ?>">
                                                 <input type="hidden" name="stock_info" value="<?php echo $row["stock"]; ?>">
                                                 
-                                                <button type="submit" name="passProductInfoBtn" id="passProductInfoBtn" class="updateProduct"><img
-                                                        src='../assets/edit.svg'></button>
+                                                <button type="submit" name="passProductInfoBtn" id="passProductInfoBtn" class="updateProduct"><img src='../assets/edit.svg'></button>
                                             </form>
                                         </td>
                                     </tr>
@@ -215,10 +203,6 @@ include ("PhpFunctions/update_product.php");
                                 <label>Net Weight</label>
                                 <input type="text" id="username" name="netWeight"><br><br>
                             </div>
-                            <!-- <div class="labelInput">
-                            <label>Product ID</label>
-                            <input type="text" id="username" required><br><br>
-                        </div> -->
                             <div class="labelInput">
                                 <label for="category">Category</label>
                                 <select id="category" name="category" required>
@@ -287,35 +271,6 @@ include ("PhpFunctions/update_product.php");
         </div>
     </div>
 
-    <!-- <php
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['passProductInfoBtn'])) {
-        $productId = $_POST['productId_info'];
-
-        echo '<script>alert("product id (php): ' .$productId .'")';
-        // Fetch data from the database
-        $select_query = "SELECT * FROM `inventory` WHERE `product_id` = '$productId'";
-        $result = mysqli_query($conn, $select_query);
-
-        // Check if the query was successful
-        if ($result && mysqli_num_rows($result) > 0) {
-            // Fetch the product details
-            $row = mysqli_fetch_assoc($result);
-            $productName_info = $row['product_name'];
-            $netWeight_info = $row['net_weight'];
-            $unit_info = $row['unit'];
-            $category_info = $row['category'];
-            $unitPrice_info = $row['unit_price'];
-            $retailPrice_info = $row['retail_price'];
-            $stock_info = $row['stock'];
-            $url_info = $row['picture_url'];
-
-        } else {
-            // No matching product found
-            echo "Product not found!";
-        }
-    }
-    ?> -->
-
     <div id="updateProductModal">
         <div class="background">
             <div class="ItemContainer">
@@ -345,13 +300,10 @@ include ("PhpFunctions/update_product.php");
                             <div class="labelInput">
                                 <label>Product Name</label>
                                 <input type="text" id="productNameInput" required><br><br>
-                                    <!-- <input type="text" id="productNameInput" name="productName" id="productNameInput"
-                                    value="<php echo $productName_info; ?>" required><br><br> -->
                             </div>
                             <div class="labelInput">
                                 <label>Net Weight</label>
-                                    <!-- <input type="text" id="netWeightInput" ><br><br> -->
-                                    <input type="text" id="netWeightInput"><br><br>
+                                <input type="text" id="netWeightInput"><br><br>
                             </div>
                             
                             <div class="labelInput">
@@ -396,26 +348,22 @@ include ("PhpFunctions/update_product.php");
                                 <label for="pricingInfo">Pricing Info</label><br><br>
                                 <div class="labelInput">
                                     <label>Unit Price</label>
-                                    <input type="text" id="unitPriceInput" name="unitPrice" id="unitPriceInput"
-                                        value="<?php echo $unitPrice; ?>" required><br><br>
+                                    <input type="text" id="unitPriceInput" name="unitPrice" id="unitPriceInput" required><br><br>
                                 </div>
                                 <div class="labelInput">
                                     <label>Retail Price</label>
-                                    <input type="text" id="retailPriceInput" name="retailPrice" id="retailPriceInput"
-                                        value="<?php echo $retailPrice; ?>" required><br><br>
+                                    <input type="text" id="retailPriceInput" name="retailPrice" id="retailPriceInput" required><br><br>
                                 </div>
                             </div>
                             <div class="stockInfo">
                                 <label for="stockInfo">Stock Info</label><br><br>
                                 <div class="labelInput">
                                     <label>Stock</label>
-                                    <input type="text" id="stockInput" name="stock" id="stockInput"
-                                        value="<?php echo $stock; ?>" required><br><br>
+                                    <input type="text" id="stockInput" name="stock" id="stockInput" required><br><br>
                                 </div>
                             </div>
                             <div class="updateButtons">
-                                <button class="addProduct" id="updateProductInfoBtn" name="updateProductInfoBtn"
-                                    type="submit">Update Product</button>
+                                <button class="addProduct" id="updateProductInfoBtn" name="updateProductInfoBtn" type="submit">Update Product</button>
                                 <button class="cancel" id="cancelUpdateBtn" name="cancelUpdateBtn">Cancel</button>
                             </div>
                         </div>
@@ -494,16 +442,16 @@ include ("PhpFunctions/update_product.php");
     </script>
 
     <script>
-        // Select all buttons with the class passProductInfoBtn
+        //selects all buttons with the class updateProduct
         var updateProductBtns = document.querySelectorAll(".updateProduct");
 
-        // Loop through each button and attach the event listener
+        //loops through each button and attach the event listener
         updateProductBtns.forEach(function(btn) {
             btn.addEventListener("click", function(event) {
-                // Prevent the default form submission behavior
+                //prevents from soubmitting the form and redirecting to update_product.php
                 event.preventDefault();
 
-                // Get the product information associated with these buttons
+                //gets the product information associated with these (from the hidden inputs)
                 var productId = this.parentNode.querySelector('[name="productId_info"]').value;
                 var productName = this.parentNode.querySelector('[name="productName_info"]').value;
                 var netWeight = this.parentNode.querySelector('[name="netWeight_info"]').value;
@@ -512,25 +460,8 @@ include ("PhpFunctions/update_product.php");
                 var unitPrice = this.parentNode.querySelector('[name="unitPrice_info"]').value;
                 var retailPrice = this.parentNode.querySelector('[name="retailPrice_info"]').value;
                 var stock = this.parentNode.querySelector('[name="stock_info"]').value;
-                
-                // Set the product info in the hidden input field
-                document.getElementById('productIdInput').value = productId;
 
-                document.getElementById('productNameInput').value = productName;
-
-                document.getElementById('netWeightInput').value = netWeight;
-
-                document.getElementById('categoryInput').value = category;
-
-                document.getElementById('unitInput').value = unit
-
-                document.getElementById('unitPriceInput').value = unitPrice;
-
-                document.getElementById('retailPriceInput').value = retailPrice;
-
-                document.getElementById('stockInput').value = stock;
-
-                // Set the product info in the input field inside the modal form
+                //sets the product info in the input fields inside the UpdateProductModal
                 var productIdInput = document.getElementById('productIdInput');
                 productIdInput.value = productId;
 
@@ -540,10 +471,10 @@ include ("PhpFunctions/update_product.php");
                 var netWeightInput = document.getElementById('netWeightInput');
                 netWeightInput.value = netWeight;
 
-                // Get the category value from the hidden input field
+                //gets the category value from the hidden input field
                 var categoryValue = document.querySelector('[name="category_info"]').value;
 
-                // Select the corresponding option in the dropdown menu
+                //selects the corresponding option in the dropdown menu
                 var categoryInput = document.getElementById('categoryInput');
                 for (var i = 0; i < categoryInput.options.length; i++) {
                     if (categoryInput.options[i].value === categoryValue) {
@@ -552,10 +483,10 @@ include ("PhpFunctions/update_product.php");
                     }
                 }
 
-                // Get the unit value from the hidden input field
+                //sets the unit value from the hidden input field
                 var unitValue = document.querySelector('[name="unit_info"]').value;
 
-                // Select the corresponding option in the dropdown menu
+                //selects the corresponding option in the dropdown menu
                 var unitInput = document.getElementById('unitInput');
                 for (var i = 0; i < unitInput.options.length; i++) {
                     if (unitInput.options[i].value === unitValue) {
@@ -573,31 +504,17 @@ include ("PhpFunctions/update_product.php");
                 var stockInput = document.getElementById('stockInput');
                 stockInput.value = stock;
 
-                // var url = document.getElementById('retailPriceInput');
-                // retailPriceInput.value = productId;
-                
-                // Show the updateProductModal
+                // var url = document.getElementById('urlInput');
+                // urlInput.value = pictureUrl;
+
                 var updateProductModal = document.getElementById("updateProductModal");
                 updateProductModal.style.display = "block";
             });
         });
 
-        // var updateProductBtn = document.getElementById("passProductInfoBtn");
-
-        // updateProductBtn.addEventListener("click", function (event) {
-        //     // Prevent the default form submission behavior
-        //     event.preventDefault();
-
-        //     var productId = this.parentNode.querySelector('[name="productId_info"]').value;
-        //     document.getElementById('productIdInput').value = productId;
-        //     var updateProductModal = document.getElementById("updateProductModal");
-        //     updateProductModal.style.display = "block";
-        // });
-
         var cancelUpdateBtn = document.getElementById("cancelUpdateBtn");
 
         cancelUpdateBtn.addEventListener("click", function (event) {
-            // Prevent the default form submission behavior
             event.preventDefault();
 
             var updateProductModal = document.getElementById("updateProductModal");
