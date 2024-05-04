52512dd34f3c0bf9db9c4f95a5b171462c2be02c
Hediphone <hediphonextra@gmail.com>
2024-05-04 20:58:00 +0800

    copied the updated items from main

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//redirect to inventory
+document.getElementById("inventoryBtn").onclick = function () {
+    window.location.href = "inventory.php";
+};
+
+//redirect to POS
+document.getElementById("POSBtn").onclick = function () {
+    window.location.href = "POS.php";
+};
