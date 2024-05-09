cd712ab2ec87195f360a2b31d98c2707b8999474
Hediphone <hediphonextra@gmail.com>
2024-05-10 01:17:48 +0800

    added code for inventory log

--- pages/PhpFunctions/add_product.php
+++ pages/PhpFunctions/add_product.php
@@ -10,23 +10,26 @@ if (isset($_POST['addNewProductBtn'])) {
     $retailPrice = $_POST['retailPrice'];
     $stock = $_POST['stock'];
 
-    if($netWeight == NULL) {
-        $url = $productName .".png";
+    if ($netWeight == NULL) {
+        $url = $productName . ".png";
 
         $insert_query = "INSERT INTO `inventory`(`product_name`, `unit`, `category`, `unit_price`, `retail_price`, `stock`, `picture_url`) VALUES ('$productName', '$unit', '$category', '$unitPrice', '$retailPrice', '$stock', '$url')";
         if (mysqli_query($conn, $insert_query)) {
+            $productId = mysqli_insert_id($conn); 
+
             echo '<script>
                     alert("Data inserted successfully to inventory."); 
                 </script>';
         } else {
             echo '<script>alert("Error inserting data: ' . mysqli_error($conn) . '");</script>';
         }
-    }
-    else {
-        $url = $productName ." " .$netWeight .".png";
+    } else {
+        $url = $productName . " " . $netWeight . ".png";
 
         $insert_query = "INSERT INTO `inventory`(`product_name`, `net_weight`, `unit`, `category`, `unit_price`, `retail_price`, `stock`, `picture_url`) VALUES ('$productName', '$netWeight', '$unit', '$category', '$unitPrice', '$retailPrice', '$stock', '$url')";
         if (mysqli_query($conn, $insert_query)) {
+            $productId = mysqli_insert_id($conn);
+            
             echo '<script>
                     alert("Data inserted successfully to inventory."); 
                 </script>';
@@ -35,12 +38,28 @@ if (isset($_POST['addNewProductBtn'])) {
         }
     }
 
-    $date = DATE_FORMAT(NOW(), '%Y-%M-%d %h:%i:%s %p');
-    $insert_query = "INSERT INTO `inventory_log`(`product_id`, `action_type`, `date`, `new_state`) VALUES ('$productId', 'Add', '$date', '$productName, $netWeight, $unit, $category, $unitPrice, $retailPrice, $stock, $url')"; 
-    if (mysqli_query($conn, $insert_query)) {
+    $previous_state = "No previous state";
+    $new_state = "Product Name: " . $productName . "\n";
+
+    if ($netWeight != NULL) {
+        $new_state .= "Net Weight: " . $netWeight . "\n";
+    }
+
+    $new_state .= "Unit: " . $unit . "\n";
+    $new_state .= "Category: " . $category . "\n";
+    $new_state .= "Unit Price: " . $unitPrice . "\n";
+    $new_state .= "Retail Price: " . $retailPrice . "\n";
+    $new_state .= "Stock: " . $stock . "\n";
+    $new_state .= "Url: " . $url;
+
+    date_default_timezone_set('Asia/Manila');
+    $date = date('Y-m-d H:i:s');
+
+    $insert = "INSERT INTO `inventory_log`(`product_id`, `action_type`, `date`, `previous_state`, `new_state`) VALUES ('$productId', 'Add', '$date', '$previous_state', '$new_state')";
+    if (mysqli_query($conn, $insert)) {
         echo '<script>
-                alert("Data inserted successfully to inventory log."); 
-                window.location.href = "../inventory.php";
+                alert("Data inserted successfully to inventory log.");
+                window.location.href = "../inventory.php"; 
             </script>';
     } else {
         echo '<script>alert("Error inserting data to inventory log: ' . mysqli_error($conn) . '");</script>';
