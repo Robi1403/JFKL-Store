e90c93482010176b07dc941a3ff9991b86bf9776
Archie Onoya <archieonoya13@gmail.com>
2024-05-15 02:01:09 +0800

    Update inventory_log.php

--- pages/inventory_log.php
+++ pages/inventory_log.php
@@ -15,9 +15,10 @@ include ("PhpFunctions/update_product.php");
     <link rel="stylesheet" href="../css/inventory.css">
     <link rel="icon" href="../assets/storeLogo.svg">
 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/daterangepicker.css" />
 
 </head>
 
@@ -103,7 +104,9 @@ include ("PhpFunctions/update_product.php");
                 <option value="remove">Remove</option>
             </select>
             
-            <input type="text" id="datepicker" placeholder="Select Date">         
+            <input type="text" id="datepicker" placeholder="Select Date"/>
+        
+
         </div>
     
 
@@ -221,9 +224,12 @@ include ("PhpFunctions/update_product.php");
 
     <script src="../js/inventory.js"></script>
 
-    <script type="text/javascript">
+    <script>
         $(function () {
-            $("#datepicker").datepicker({
+            $("#datepicker").daterangepicker({
+                singleDatePicker: true,
+                showDropdowns: true,
+                minYear: 2024,
                 onSelect: function (dateText, inst) {
                     $("#selectedDate").val(dateText);
                     $("#dateForm").submit();
@@ -231,6 +237,7 @@ include ("PhpFunctions/update_product.php");
             });
         });
     </script>
+
 
 </body>
 
