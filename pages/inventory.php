d43c73c92de4803746bf74085e85ce6313f86a8b
Archie Onoya <archieonoya13@gmail.com>
2024-05-15 02:00:59 +0800

    Update inventory.php

--- pages/inventory.php
+++ pages/inventory.php
@@ -15,6 +15,11 @@ include ("PhpFunctions/update_product.php");
     <link rel="stylesheet" href="../css/inventory.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../css/modalConfirm.css">
     <link rel="icon" href="../assets/storeLogo.svg">
+
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 </head>
 
 <body>
