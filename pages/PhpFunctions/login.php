8a25cbb5cb99cc7db22708665713011c9524f880
Robi1403 <rodejojohnrobert@gmail.com>
2024-05-16 23:44:43 +0800

    fixed login

--- pages/PhpFunctions/login.php
+++ pages/PhpFunctions/login.php
@@ -1,22 +1,17 @@
 <?php
-    session_start();
     include("connection.php");
-
-
     if (isset($_POST['submit'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $sql = "select * from user where username = '$username' and password = '$password'";
+        $sql = "SELECT * from user where username = '$username' and password = '$password'";
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
 
         if ($count == 1) {
-            if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
-                header("Location: ../POS.php");
-                exit;
-            }
+            $_SESSION['loggedin'] = true;
+            header("Location: ../POS.php");
            
         }else {
             echo `<script>
