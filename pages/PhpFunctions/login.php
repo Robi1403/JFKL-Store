bdab4c6241f23b683a79e2c698960799e2988d7a
Archie Onoya <archieonoya13@gmail.com>
2024-05-17 15:38:12 +0800

    Update login.php

--- pages/PhpFunctions/login.php
+++ pages/PhpFunctions/login.php
@@ -1,39 +1,30 @@
 <?php
 session_start();
-    include("connection.php");
-    if (isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+include("connection.php");
 
-        $sql = "SELECT * from user where username = '$username' and password = '$password'";
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $count = mysqli_num_rows($result);
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-        if ($count == 1) {
-            $_SESSION['loggedin'] = true;
-            header("Location: ../POS.php");
-           
-        }else {
-            echo `<script>
-            window.location.href = "../LoginPage.php";
-            alert("Login Failed");
-            </script>`;
-            header("Location: ../LoginPage.php");
-        }
+    $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
 
-
+    if ($count == 1) {
+        $_SESSION['loggedin'] = true;
+        header("Location: ../POS.php");
+        exit();
+    } else {
+        $_SESSION['login_error'] = "User Not Found";
+        header("Location: ../LoginPage.php");
+        exit();
     }
+}
 
-    if (isset($_GET['action'])) {
-       
-        if ($_GET['action'] == "logout") {
-         
-            unset($_SESSION['loggedin']);
-        }
-
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == "logout") {
+        unset($_SESSION['loggedin']);
     }
-    
-
-    
+}
 ?>
